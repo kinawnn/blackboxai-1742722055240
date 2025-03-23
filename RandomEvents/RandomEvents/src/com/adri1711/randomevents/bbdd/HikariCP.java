5af6c1949ac22cb49c9ec7de6b28891012ef1e4d
Kinawn <kinawn@outlook.com>
2025-03-23 16:29:50 +0700

    Add RandomEvents/RandomEvents/src/com/adri1711/randomevents/bbdd/HikariCP.java 1742722191997

---
+++ RandomEvents/RandomEvents/src/com/adri1711/randomevents/bbdd/HikariCP.java
@@ -0,0 +1,44 @@
+package com.adri1711.randomevents.bbdd;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+public class HikariCP {
+
+	private HikariDataSource hikari;
+
+	public HikariCP(String ip, String port, String database, String username, String password, Integer maxLifeTime) {
+		super();
+
+		hikari = new HikariDataSource();
+		
+		hikari.setJdbcUrl("jdbc:mysql://"+ip+":"+port+"/"+database);
+		hikari.setDriverClassName("com.mysql.jdbc.Driver");
+		hikari.addDataSourceProperty("serverName", ip);
+		hikari.addDataSourceProperty("port", port);
+		hikari.addDataSourceProperty("databaseName", database);
+		hikari.addDataSourceProperty("user", username);
+		hikari.addDataSourceProperty("password", password);
+		hikari.addDataSourceProperty("useSSL", false);
+		
+		
+		hikari.setMinimumIdle(0);
+		hikari.setConnectionTimeout(30000);
+		hikari.setIdleTimeout(35000);
+		hikari.setMaxLifetime(maxLifeTime);
+	}
+
+	public void close() {
+		if (hikari != null)
+			hikari.close();
+	}
+
+	public HikariDataSource getHikari() {
+		return hikari;
+	}
+
+	public void setHikari(HikariDataSource hikari) {
+		this.hikari = hikari;
+	}
+	
+
+}
