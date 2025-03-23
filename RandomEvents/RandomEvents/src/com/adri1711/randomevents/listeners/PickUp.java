7f768c1f02a56e3d31062389f2e13b7f13e9b1a7
Kinawn <kinawn@outlook.com>
2025-03-23 16:28:50 +0700

    Add RandomEvents/RandomEvents/src/com/adri1711/randomevents/listeners/PickUp.java 1742722131869

---
+++ RandomEvents/RandomEvents/src/com/adri1711/randomevents/listeners/PickUp.java
@@ -0,0 +1,72 @@
+package com.adri1711.randomevents.listeners;
+
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+import com.adri1711.randomevents.RandomEvents;
+import com.adri1711.randomevents.util.Constantes;
+import com.adri1711.randomevents.util.UtilsRandomEvents;
+import com.adri1711.util.enums.XMaterial;
+
+public class PickUp implements Listener {
+
+	private RandomEvents plugin;
+
+	public PickUp(RandomEvents plugin) {
+		super();
+		this.plugin = plugin;
+	}
+
+	@EventHandler
+	public void onPickUp(PlayerPickupItemEvent evt) {
+		Player player = evt.getPlayer();
+		if (plugin.getMatchActive() != null
+				&& plugin.getMatchActive().getPlayerHandler().getPlayersObj().contains(player)) {
+
+			switch (plugin.getMatchActive().getMatch().getMinigame()) {
+			case GEM_CRAWLER:
+				Item item = evt.getItem();
+				
+				if (item.getItemStack().getType().equals(XMaterial.EMERALD.parseMaterial())) {
+					if (plugin.getMatchActive().getPuntuacion().containsKey(player.getName())) {
+						plugin.getMatchActive().getPuntuacion().put(player.getName(),
+								plugin.getMatchActive().getPuntuacion().get(player.getName())
+										+ item.getItemStack().getAmount());
+
+					} else {
+						plugin.getMatchActive().getPuntuacion().put(player.getName(), item.getItemStack().getAmount());
+					}
+
+					item.remove();
+					if (plugin.getMatchActive().getPlayerHandler().getPlayerContador() == null) {
+						UtilsRandomEvents.mandaMensaje(plugin,
+								plugin.getMatchActive().getPlayerHandler().getPlayersObj(),
+								plugin.getLanguage().getNowGems()
+										.replace("%player%", player.getName()).replace("%points%", plugin
+												.getMatchActive().getPuntuacion().get(player.getName()).toString()),
+								true);
+						plugin.getMatchActive().compruebaPartida();
+					}
+					plugin.getMatchActive().updateScoreboards();
+					evt.setCancelled(true);
+				}
+				break;
+			default:
+				break;
+			}
+		}
+
+	}
+
+	public RandomEvents getPlugin() {
+		return plugin;
+	}
+
+	public void setPlugin(RandomEvents plugin) {
+		this.plugin = plugin;
+	}
+
+}
